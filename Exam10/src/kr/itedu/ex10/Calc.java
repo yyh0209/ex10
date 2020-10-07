698a25bfd53c43236be2f3e55426c652463b8673
Administrator <Administrator@DESKTOP-ISVFPRU>
2020-10-07 11:22:41 +0900

    minus메소드 추가

--- Exam10/src/kr/itedu/ex10/Calc.java
+++ Exam10/src/kr/itedu/ex10/Calc.java
@@ -1,8 +1,12 @@
 package kr.itedu.ex10;
 
 public class Calc {
-	//n1, n2를 더한 값을 리턴해야 함
+	// n1, n2를 더한 값을 리턴해야 함
 	public static int sum(int n1, int n2) {
 		return n1 + n2;
-	}	
+	}
+
+	public static int minus(int n1, int n2) {
+		return n1 - n2;
+	}
 }
